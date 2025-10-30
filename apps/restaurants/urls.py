f53c2a6988739b2ae5bb87546a4fb8218cb49a70
adubiblessed <adubiblessed@gmail.com>
2025-10-30 20:33:19 +0100

    added api route for each restaurant

--- apps/restaurants/urls.py
+++ apps/restaurants/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RestaurantsApiView, AllRestaurantsApiView
+from .views import RestaurantsApiView, AllRestaurantsApiView, EachRestaurantApiView
 
 urlpatterns = [
     path('restaurants/', RestaurantsApiView.as_view(), name='restaurant-list'),
     path('all/', AllRestaurantsApiView.as_view(), name='all-restaurant-list'),
+    path('<str:pk>/', EachRestaurantApiView.as_view(), name='restaurant-detail'),
 ]
--- apps/restaurants/views.py
+++ apps/restaurants/views.py
@@ -27,3 +27,37 @@ class AllRestaurantsApiView(APIView):
         restaurants = Restaurant.objects.all()
         serializer = RestaurantSerializer(restaurants, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class EachRestaurantApiView(APIView):
+    authentication_classes=[TokenAuthentication]
+    
+    def get_object(self, pk, user):
+        try:
+            return Restaurant.objects.get(pk=pk, owner=user)
+        except Restaurant.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        restaurant = self.get_object(pk, request.user)
+        if not restaurant:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = RestaurantSerializer(restaurant)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        restaurant = self.get_object(pk, request.user)
+        if not restaurant:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = RestaurantSerializer(restaurant, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        restaurant = self.get_object(pk, request.user)
+        if not restaurant:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        restaurant.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
